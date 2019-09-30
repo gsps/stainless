093b14a5b36668d4537afb42eaecfe2f722fa47d
Georg Schmid <georg.schmid@epfl.ch>
2019-09-30 17:03:52 +0200

    Extended ListOps example (currently breaks)

--- frontends/benchmarks/rustgen/valid/ListOps.scala
+++ frontends/benchmarks/rustgen/valid/ListOps.scala
@@ -1,4 +1,5 @@
 /* Copyright 2009-2019 EPFL, Lausanne */
+import stainless.lang.error
 import stainless.rust._
 
 object ListOps {
@@ -12,8 +13,23 @@ object ListOps {
       case Cons(_, xs) => 1 + length(xs)
     }
 
+  def head(xs: List): Int =
+    xs match {
+      case Nil() => error("Cannot take head of empty list")
+      case Cons(x, _) => x
+    }
+
+  def tail(xs: List): List =
+    xs match {
+      case Nil() => error("Cannot take tail of empty list")
+      case Cons(_, xs) => xs
+    }
+
   def main(): Unit = {
     val xs = Cons(1, Cons(2, Nil()))
     println(length(xs))
+    println(head(xs))
+    println(head(tail(xs)))
+    println(head(tail(tail(xs))))
   }
 }
